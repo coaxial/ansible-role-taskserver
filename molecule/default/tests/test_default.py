034b9ae2ef2efcbd10e50b049be527eb3ffc1266
coaxial <hi@64b.it>
2018-06-06 09:59:46 -0400

    heyoooo

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -79,16 +79,16 @@ def test_ssh_files(host):
 
 def test_restoration(host):
     taskserver_container_name = host.run(
-        "sudo docker ps -f 'name=service_taskserver'"
+        "sudo docker ps -f 'name=service_taskserver' "
         " --format={{'{{.Names}}'}}"
     )
     taskserver_ip_cmd = (
         "sudo docker inspect -f "
         "{{'{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'}} %s"
-        % taskserver_container_name.stdout
+        % taskserver_container_name['stdout']
     )
     taskserver_ip = host.run(taskserver_ip_cmd)
-    task_list_cmd = "docker run --rm --add-host taskd.example.com:%s" \
+    task_list_cmd = "docker run --rm --add-host taskd.example.com:%s " \
         "-v `pwd`/molecule/default/client_files:/client_files:ro alpine sh -c"\
         " 'ping -c 3 taskd.example.com && apk --no-cache add task && " \
         "yes | task version &&" \
@@ -98,7 +98,7 @@ def test_restoration(host):
         "yes | task config taskd.server taskd.example.org:53589 &&" \
         "yes | task config taskd.credentials -- " \
         "My Org/user/$(cat /client_files/user-uuid) &&" \
-        "yes | task sync init'" % taskserver_ip.stdout
+        "yes | task sync init'" % taskserver_ip['stdout']
 
     tasks = host.run(task_list_cmd)
 
