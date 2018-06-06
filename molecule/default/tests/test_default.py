c6b6cf5a10b601fbd08f07cb2844e22c0385cf28
coaxial <hi@64b.it>
2018-06-06 12:21:53 -0400

    hlhglf

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -80,7 +80,7 @@ def test_ssh_files(host):
 def test_restoration(host):
     # cf http://jinja.pocoo.org/docs/2.10/templates/#escaping
     taskserver_container_name_cmd = {{
-        'docker ps -f "name=service_taskserver" --format="{{.Names}}"'
+        'docker ps -f "name=service_taskserver" --format={{.Names}}'
     }}
     taskserver_container_name = host.check_output(
         taskserver_container_name_cmd
