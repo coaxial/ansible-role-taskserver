ee81cd0e07e0f47bb2af4c6ffb4faf297715005f
Pierre <coaxial@users.noreply.github.com>
2018-06-10 09:29:24 -0400

    Start docker containers on Travis (#5)
    * User docker pip module and GCE
    
    * Use solita image
    
    * Disable testing var
    
    * Install pip packages
    
    * Use borg server
    
    * Run molecule in debug mode
    
    * Make fs rw
    
    * Create missing dir
    
    * debug
    
    * Fix borg url
    
    * Add fingerprint to known_hosts
    
    * debug
    
    * debug
    
    * Fingerprint host
    
    * Host key issues
    
    * debug
    
    * Use port 2222
    
    * Fix hostname
    
    * Fix key name
    
    * Lint
    
    * Wait longer for borgserver to start
    
    * debug
    
    * Remove line return
    
    * debug
    
    * debug
    
    * who knows
    
    * lala
    
    * debug
    
    * Fix port
    
    * Fix port
    
    * Fix repo name
    
    * debug
    
    * Rename repo
    
    * Test server
    
    * Fix ip
    
    * debug
    
    * lalala
    
    * skip idempotence
    
    * Cleanup
    
    * Test sync
    
    * Test restoration
    
    * Fix lint
    
    * wip
    
    * Change jinja2 interpolation string
    
    * Amazing jinja2
    
    * ...
    
    * jinja is fun
    
    * youpi
    
    * yo
    
    * plip
    
    * heyoooo
    
    * Use check_output
    
    * Escape quotes
    
    * Escape whole string
    
    * Line continuation
    
    * really ,# jinja
    
    * Remove unnecessary sudo
    
    * lalala
    
    * fjkdsalfhdlkjsghlasfhglasgh
    
    * gflglfdhgldf
    
    * gjklershgksdrhglrehgoregiosarh
    
    * hlhglf
    
    * ghflhgfd
    
    * booya
    
    * ghfklhgkldfhgldsfhglhsdfg
    
    * lfhglfg
    
    * gdflgfls
    
    * find network
    
    * Join network
    
    * Use hosts file
    
    * spaces spaces spaces
    
    * debug
    
    * debug
    
    * Fix volume path
    
    * debug
    
    * debug
    
    * Fix vol path
    
    * diag
    
    * debug
    
    * debug
    
    * debug
    
    * debug restore
    
    * yay
    
    * device is not a tty
    
    * no tty
    
    * gfklhglf
    
    * ofc no comments
    
    * ghflhgldgf
    
    * debug
    
    * debug
    
    * Fix orgname
    
    * debug
    
    * verbose
    
    * Fix hostname mismatch
    
    * add hostname to hostsfile
    
    * ignore borgrepo
    
    * check if taskserver is reachable
    
    * heh
    
    * fix addresses
    
    * typo
    
    * Delete exisiting taskddata before restoring
    
    * show tasks
    
    * set trust
    
    * that was easy...
    
    * Cleanup
    
    * Remove debug command
    
    * Add details to warning
    
    * Give steps names

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -75,3 +75,52 @@ def test_ssh_files(host):
     for f in files:
         assert host.file(borgmatic_dir + '/ssh/' + f).exists
         assert host.file(borgmatic_dir + '/ssh/' + f).mode == 0o600
+
+
+def test_restoration(host):
+    # first extract container name and ip, then start a new container and
+    # install taskwarrior on it, configure it, and attempt to sync with
+    # taskserver from role.
+
+    # cf http://jinja.pocoo.org/docs/2.10/templates/#escaping
+    taskserver_container_name_cmd = (
+        "docker ps -f 'name=service_taskserver' "
+        "{% raw -%}--format='{{.Names}}'{% endraw -%}"
+    )
+    taskserver_container_name = host.check_output(
+        taskserver_container_name_cmd
+    )
+    # cf http://jinja.pocoo.org/docs/2.10/templates/#escaping
+    inspect_format_string = (
+        "{% raw -%}{{range .NetworkSettings.Networks}}"
+        "{{.IPAddress}}{{end}}{% endraw -%}"
+    )
+    taskserver_ip_cmd = (
+        "docker inspect -f '%s' %s"
+        % (inspect_format_string, taskserver_container_name)
+    )
+    taskserver_ip = host.check_output(taskserver_ip_cmd)
+    task_list_cmd = (
+        "docker run --rm --network container:%s "
+        "-v /opt/docker-taskd-service/taskserver/client_certs/:"
+        "/client_files:ro alpine sh "
+        "-c '"
+        "echo \"%s taskserver\" >> /etc/hosts && "
+        "apk --no-cache add task && "
+        "yes | task version && "
+        "yes | task config taskd.ca /client_files/ca.cert.pem && "
+        "yes | task config taskd.certificate /client_files/user.cert.pem && "
+        "yes | task config taskd.key /client_files/user.key.pem && "
+        "yes | task config taskd.server taskserver:53589 && "
+        "yes | task config taskd.credentials -- "
+        "My Org/user/$(cat /client_files/user-uuid) && "
+        "yes | task sync init"
+        "'"
+        % (
+            taskserver_container_name, taskserver_ip
+        )
+    )
+
+    task_sync = host.run(task_list_cmd)
+
+    assert "Sync successful." in task_sync.stderr
